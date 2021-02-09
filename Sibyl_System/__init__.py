63fbaa02e8e9c168d17e7ad524898b076403be50
Zeref_Dragneel <ZerefDev@gmail.com>
2021-02-09 14:30:24 +0530

    Update __init__.py

--- Sibyl_System/__init__.py
+++ Sibyl_System/__init__.py
@@ -38,24 +38,7 @@ if ENV:
     GBAN_MSG_LOGS = int(os.environ.get("GBAN_MSG_LOGS"))
     BOT_TOKEN = os.environ.get("BOT_TOKEN")
 else:
-    import Sibyl_System.config as Config
-
-    API_ID_KEY = Config.API_ID
-    API_HASH_KEY = Config.API_HASH
-    STRING_SESSION = Config.STRING_SESSION
-    MONGO_DB_URL = Config.MONGO_DB_URL
-    with open(os.path.join(os.getcwd(), "Sibyl_System\\elevated_users.json"), "r") as f:
-        data = json.load(f)
-    SIBYL = data["SIBYL"]
-    ENFORCERS = data["ENFORCERS"]
-    INSPECTORS = data["INSPECTORS"]
-    Sibyl_logs = Config.Sibyl_logs
-    Sibyl_approved_logs = Config.Sibyl_approved_logs
-    GBAN_MSG_LOGS = Config.GBAN_MSG_LOGS
-    BOT_TOKEN = Config.BOT_TOKEN
-
-INSPECTORS.extend(SIBYL)
-ENFORCERS.extend(INSPECTORS)
+    None
 
 
 
